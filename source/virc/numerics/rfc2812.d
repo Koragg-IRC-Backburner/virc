fd7ba04ee5b6b954f62950ec868479fb9b301f0b
Cameron Ross <elpenguino@gmail.com>
2017-05-29 18:41:03 -0300

    add RPL_TRACESERVICE and RPL_TRACECLASS. extremely basic support.

--- source/virc/numerics/rfc2812.d
+++ source/virc/numerics/rfc2812.d
@@ -3,7 +3,10 @@
 +/
 module virc.numerics.rfc2812;
 
+import std.range : ElementType;
+
 import virc.numerics.definitions;
+
 /++
 + 004 RPL_MYINFO response.
 +/
@@ -80,5 +83,144 @@ auto parseNumeric(Numeric numeric : Numeric.RPL_MYINFO, T)(T input) {
 		assert(info.channelModesWithParams == "FIJLYabefghjkloqv");
 		assert(info.serverModes == "");
 		assert(info.serverModesWithParams == "");
+	}
+}
+/++
++ Parser for RPL_TRACESERVICE.
++
++ Format is `207 <client> Service <class> <name> <type> <active_type>`
++/
+auto parseNumeric(Numeric numeric : Numeric.RPL_TRACESERVICE, T)(T input) if (is(ElementType!T : string)) {
+	import std.range : empty, front, popFront;
+	import std.typecons : Nullable, Tuple;
+	Tuple!(string, "class_", string, "name", string, "type", string, "activeType") output;
+	//Drop client token
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	input.popFront();
+	//Drop Service token
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	input.popFront();
+
+
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	output.class_ = input.front;
+	input.popFront();
+
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	output.name = input.front;
+	input.popFront();
+
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	output.type = input.front;
+	input.popFront();
+
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	output.activeType = input.front;
+	return Nullable!(typeof(output))(output);
+}
+///
+@safe pure @nogc nothrow unittest {
+	import std.range : only, takeNone;
+	//Need real world examples... No idea what these will really look like
+	{
+		immutable trace = parseNumeric!(Numeric.RPL_TRACESERVICE)(only("someone", "Service", "classy", "fred", "something", "no_idea"));
+		assert(trace.class_ == "classy");
+		assert(trace.name == "fred");
+		assert(trace.type == "something");
+		assert(trace.activeType == "no_idea");
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACESERVICE)(takeNone!(string[]));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACESERVICE)(only("someone"));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACESERVICE)(only("someone", "Service"));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACESERVICE)(only("someone", "Service", "classy"));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACESERVICE)(only("someone", "Service", "classy", "fred"));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACESERVICE)(only("someone", "Service", "classy", "fred", "something"));
+		assert(badTrace.isNull);
+	}
+}
+/++
++ Parser for RPL_TRACECLASS.
++
++ Format is `209 <client> Class <class> <count>`
++/
+auto parseNumeric(Numeric numeric : Numeric.RPL_TRACECLASS, T)(T input) {
+	import std.range : empty, front, popFront;
+	import std.typecons : Nullable, Tuple;
+	Tuple!(string, "class_", string, "count") output;
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	//Drop client token
+	input.popFront();
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	//Drop Class token
+	input.popFront();
+
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	output.class_ = input.front;
+	input.popFront();
+
+	if (input.empty) {
+		return Nullable!(typeof(output)).init;
+	}
+	output.count = input.front;
+	return Nullable!(typeof(output))(output);
+}
+///
+@safe pure unittest {
+	import std.range : only, takeNone;
+	//Need real world examples... No idea what these will really look like
+	{
+		immutable trace = parseNumeric!(Numeric.RPL_TRACECLASS)(only("someone", "CLASS", "classy", "238525813"));
+		assert(trace.class_ == "classy");
+		assert(trace.count == "238525813");
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACECLASS)(takeNone!(string[]));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACECLASS)(only("someone"));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACECLASS)(only("someone", "Class"));
+		assert(badTrace.isNull);
+	}
+	{
+		immutable badTrace = parseNumeric!(Numeric.RPL_TRACECLASS)(only("someone", "Class", "classy"));
+		assert(badTrace.isNull);
 	}
 }
