e944585623d710aac4f0599afefecb08e94b3406
Cameron Ross <elpenguino@gmail.com>
2017-06-08 00:22:24 -0300

    fix a bug, the docs and a test for RPL_WATCH numerics

--- source/virc/numerics/watch.d
+++ source/virc/numerics/watch.d
@@ -7,19 +7,31 @@ import std.algorithm.comparison : among;
 
 import virc.numerics.definitions;
 /++
++ Parses most of the WATCH numerics. Most of them have the same format, so the
++ majority of the magic happens in this function.
 +
++ Formats are:
++
++ `600 <client> <nickname> <username> <hostname> <signontime> :logged on`
++
++ `601 <client> <nickname> <username> <hostname> <lastnickchange> :logged off`
++
++ `602 <client> <nickname> <username> <hostname> <lastnickchange> :stopped watching`
++
++ `604 <client> <nickname> <username> <hostname> <lastnickchange> :is online`
++
++ `605 <client> <nickname> <username> <hostname> <lastnickchange> :is offline`
++
++ `609 <client> <nickname> <username> <hostname> <awaysince> :is away`
++
++ Standards: Conforms to https://raw.githubusercontent.com/grawity/irc-docs/master/client/draft-meglio-irc-watch-00.txt
 +/
-//600 <nickname> <username> <hostname> <signontime> :logged on
-//601 <nickname> <username> <hostname> <lastnickchange> :logged off
-//602 <nickname> <username> <hostname> <lastnickchange> :stopped watching
-//604 <nickname> <username> <hostname> <lastnickchange> :is online
-//605 <nickname> <username> <hostname> <lastnickchange> :is offline
-//609 <nickname> <username> <hostname> <awaysince> :is away
 auto parseNumeric(Numeric numeric, T)(T input) if (numeric.among(Numeric.RPL_LOGON, Numeric.RPL_LOGOFF, Numeric.RPL_WATCHOFF, Numeric.RPL_NOWOFF, Numeric.RPL_NOWON, Numeric.RPL_NOWISAWAY)) {
 	import std.conv : to;
 	import std.datetime : SysTime, UTC;
 	import std.typecons : tuple;
 	import virc.common : User;
+	input.popFront();
 	auto user = User();
 	user.mask.nickname = input.front;
 	input.popFront();
@@ -35,8 +47,8 @@ auto parseNumeric(Numeric numeric, T)(T input) if (numeric.among(Numeric.RPL_LOG
 	import std.datetime : DateTime, SysTime, UTC;
 	import std.range : only;
 	{
-		immutable logon = parseNumeric!(Numeric.RPL_LOGON)(only("someone", "someIdent", "example.net", "911248013", "logged on"));
-		assert(logon.user.mask.nickname == "someone");
+		immutable logon = parseNumeric!(Numeric.RPL_LOGON)(only("someone", "someoneElse", "someIdent", "example.net", "911248013", "logged on"));
+		assert(logon.user.mask.nickname == "someoneElse");
 		assert(logon.user.mask.ident == "someIdent");
 		assert(logon.user.mask.host == "example.net");
 		static immutable date = SysTime(DateTime(1998, 11, 16, 20, 26, 53), UTC());
